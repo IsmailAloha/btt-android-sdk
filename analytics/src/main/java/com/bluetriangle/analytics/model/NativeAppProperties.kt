1f07f7c2bd41ef021dd84c574ca1e569ed7d1f67
Ismail <ismails@alohatechnologydev.com>
2023-11-01 14:25:10 +0530

    NativeAppProperties code reformatted
    Signed-off-by: Ismail <ismails@alohatechnologydev.com>

--- analytics/src/main/java/com/bluetriangle/analytics/model/NativeAppProperties.kt
+++ analytics/src/main/java/com/bluetriangle/analytics/model/NativeAppProperties.kt
@@ -7,16 +7,16 @@ import org.json.JSONObject
 
 @Parcelize
 internal data class NativeAppProperties(
-    var loadTime:Long?=null,
-    var fullTime:Long?=null,
-    var maxMainThreadUsage:Long?=null,
-    var screenType:ScreenType?=null,
-    var numberOfCPUCores:Long?=null,
+    var loadTime: Long? = null,
+    var fullTime: Long? = null,
+    var maxMainThreadUsage: Long? = null,
+    var screenType: ScreenType? = null,
+    var numberOfCPUCores: Long? = null,
     var wifi: Long? = null,
     var cellular: Long? = null,
     var ethernet: Long? = null,
     var offline: Long? = null
-):Parcelable {
+) : Parcelable {
     fun toJSONObject(): JSONObject {
         val obj = JSONObject()
         obj.put("loadTime", loadTime)
@@ -24,37 +24,26 @@ internal data class NativeAppProperties(
         obj.put("maxMainThreadUsage", maxMainThreadUsage)
         obj.put("screenType", screenType?.name)
         obj.put("numberOfCPUCores", numberOfCPUCores)
+
         var max = Long.MIN_VALUE
         var maxField = ""
-        if (wifi != null && wifi != 0L) {
-            obj.put("wifi", wifi)
-            if(wifi!! > max) {
-                max = wifi!!
-                maxField = "wifi"
+
+        val putAndCalculateMax:(Long?, String) -> Unit = { field, name ->
+            if (field != null && field != 0L) {
+                obj.put(name, field)
+                if (field > max) {
+                    max = field
+                    maxField = name
+                }
             }
         }
-        if (cellular != null && cellular != 0L) {
-            obj.put("cellular", cellular)
-            if(cellular!! > max) {
-                max = cellular!!
-                maxField = "cellular"
-            }
-        }
-        if (ethernet != null && ethernet != 0L) {
-            obj.put("ethernet", ethernet)
-            if(ethernet!! > max) {
-                max = ethernet!!
-                maxField = "ethernet"
-            }
-        }
-        if (offline != null && offline != 0L) {
-            obj.put("offline", offline)
-            if(offline!! > max) {
-                max = offline!!
-                maxField = "offline"
-            }
-        }
-        if(max != Long.MIN_VALUE) {
+
+        putAndCalculateMax(wifi, "wifi")
+        putAndCalculateMax(cellular, "cellular")
+        putAndCalculateMax(ethernet, "ethernet")
+        putAndCalculateMax(offline, "offline")
+
+        if (maxField.isNotEmpty()) {
             obj.put(CapturedRequest.FIELD_NETWORK_STATE, maxField)
         }
         return obj
