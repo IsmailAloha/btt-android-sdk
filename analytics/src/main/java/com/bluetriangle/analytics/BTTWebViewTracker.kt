c4135132214bf7e9798f75f7ce8c7bd6f4e2cb88
Ismail <ismails@alohatechnologydev.com>
2024-01-12 20:04:14 +0530

    Added BTTWebTracker class
    Signed-off-by: Ismail <ismails@alohatechnologydev.com>

---
+++ analytics/src/main/java/com/bluetriangle/analytics/BTTWebViewTracker.kt
@@ -0,0 +1,28 @@
+package com.bluetriangle.analytics
+
+import android.os.Build
+import android.webkit.WebView
+
+object BTTWebViewTracker {
+
+    @JvmStatic
+    public fun onLoadResource(view: WebView, url: String) {
+        val fileName = url.split("/").lastOrNull { segment -> segment.isNotEmpty() }
+        if(fileName != "btt.js") return
+
+        Tracker.instance?.configuration?.sessionId?.let {
+            val expiration = (System.currentTimeMillis() + 1800000).toString()
+            val setSession = "{\"value\":\"$it\",\"expires\":\"$expiration\"}"
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                view.evaluateJavascript(
+                    "window.localStorage.setItem('BTT_X0siD', '$setSession');"
+                ) {
+                    Tracker.instance?.configuration?.logger?.info("Injected SessionID in WebView: $setSession, $it")
+                }
+            } else {
+                view.loadUrl("javascript:window.localStorage.setItem('BTT_X0siD', '$setSession');")
+            }
+        }
+    }
+
+}
