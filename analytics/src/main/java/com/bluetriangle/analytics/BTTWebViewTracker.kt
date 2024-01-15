cdde0dc35096e9487685d65ac95d50b25972b57c
Ismail <ismails@alohatechnologydev.com>
2024-01-15 16:47:05 +0530

    Added SDK Version in WebView local storage
    Signed-off-by: Ismail <ismails@alohatechnologydev.com>

--- analytics/src/main/java/com/bluetriangle/analytics/BTTWebViewTracker.kt
+++ analytics/src/main/java/com/bluetriangle/analytics/BTTWebViewTracker.kt
@@ -7,22 +7,26 @@ object BTTWebViewTracker {
 
     @JvmStatic
     public fun onLoadResource(view: WebView?, url: String?) {
-        if(view == null || url == null) return
+        if (view == null || url == null) return
         val fileName = url.split("/").lastOrNull { segment -> segment.isNotEmpty() }
-        if(fileName != "btt.js") return
+        if (fileName != "btt.js") return
 
         Tracker.instance?.configuration?.sessionId?.let {
             val expiration = (System.currentTimeMillis() + 1800000).toString()
-            val setSession = "{\"value\":\"$it\",\"expires\":\"$expiration\"}"
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-                view.evaluateJavascript(
-                    "window.localStorage.setItem('BTT_X0siD', '$setSession');"
-                ) {
-                    Tracker.instance?.configuration?.logger?.info("Injected SessionID in WebView: $setSession, $it")
-                }
-            } else {
-                view.loadUrl("javascript:window.localStorage.setItem('BTT_X0siD', '$setSession');")
-            }
+            val sessionID = "{\"value\":\"$it\",\"expires\":\"$expiration\"}"
+            val sdkVersion = "{\"value\":\"Android-${BuildConfig.SDK_VERSION}\",\"expires\":\"$expiration\"}"
+
+            view.runJS("window.localStorage.setItem('BTT_X0siD', '$sessionID');")
+            view.runJS("window.localStorage.setItem('BTT_SDK_VER', '$sdkVersion');")
+            Tracker.instance?.configuration?.logger?.info("Injected session ID and SDK version in WebView: BTT_X0siD: $sessionID, BTT_SDK_VER: $sdkVersion")
+        }
+    }
+
+    private fun WebView.runJS(js: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            evaluateJavascript(js, null)
+        } else {
+            loadUrl("javascript:$js")
         }
     }
 
